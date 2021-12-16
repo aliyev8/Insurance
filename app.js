33a86db44bda4408202f5b93287939e865bfb1ee
Allahyar <81466577+aliyev8@users.noreply.github.com>
2021-12-16 16:17:45 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// const target = document.querySelectorAll("b-3");
+// const callback = (entries) => {
+//     for (const entry of entries) {
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("active");
+//         }
+
+//     };
+
+
+// }
+
+// const observer = new IntersectionObserver(callback);
+// target.forEach((member) => observer.observe(member))
